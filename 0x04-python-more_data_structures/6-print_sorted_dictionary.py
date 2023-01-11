4587b44b508b5da3148ff99721fe675742c35b02
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2023-01-11 09:30:02 -0800

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,6 @@
-aa
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    if len(a_dictionary) != 0:
+        sorted_list = sorted(a_dictionary.keys())
+        for i in sorted_list:
+            print("{}: {}".format(i, a_dictionary[i]))
