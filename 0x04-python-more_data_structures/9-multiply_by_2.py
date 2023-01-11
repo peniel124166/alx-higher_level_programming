a3bf6095234cebd006274fa8ff87402c5aa1bab5
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2023-01-11 09:35:04 -0800

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
-aa
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    for i in a_dictionary:
+        new_dictionary[i] = (a_dictionary[i] * 2)
+    return new_dictionary
