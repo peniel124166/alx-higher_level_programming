42ec860d95a34ae207ab8dc8d389bb799ca518c0
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2023-02-07 04:24:38 -0800

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
+""" Module 0-read_file
+that contains a function that reads from a file
+"""
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """ Function that reads from a file
+    Args:
+        filename: filename
+    Raises
+        Exception: when the file can be opened
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
