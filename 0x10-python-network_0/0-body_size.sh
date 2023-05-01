3998645379fd4eda0e4826d8ff4d603f3ecb4bc0
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2023-05-01 00:44:04 -0700

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# body size
-curl -sI $1 | grep "Content-Length:" | cut -d' ' -f2
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
