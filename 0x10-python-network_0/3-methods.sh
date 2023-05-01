24e7381845abdae2f681f7a90f822af9feef72d8
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2023-05-01 01:32:00 -0700

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# show all possible methods
-curl -sI -X OPTIONS $1 | grep 'Allow:' | cut -d ' ' -f2-
+# display all HTTP methods the server will accept using curl.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
