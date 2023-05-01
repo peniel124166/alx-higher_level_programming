df51606cf41eb30e2bdde50dd9bf529d7edc5782
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2023-05-01 01:40:32 -0700

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-"""
-Finds a peak in a list of unsorted integers
+""" Finds a peak in a list of unsorted integers
 """
 
 
 def find_peak(list_of_integers):
     """
-    finds num that's greater than both left and right
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
     """
-    if not list_of_integers:
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
 
-    start = 0
-    end = len(list_of_integers)-1
-
-    if list_of_integers[start] > list_of_integers[start+1]:
-        return list_of_integers[start]
-    if list_of_integers[end] > list_of_integers[end-1]:
-        return list_of_integers[end]
-
-    mid = len(list_of_integers) // 2
-    mid_val = list_of_integers[mid]
-
-    if list_of_integers[mid - 1] < mid_val \
-            and list_of_integers[mid + 1] < mid_val:
-        return mid_val
-    if mid_val < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[start:mid + 1])
-    elif mid_val < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:end + 1])
-    else:
-        return list_of_integers[start]
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
